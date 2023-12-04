6e45a0332906be2e03e2b3c937cb783bec67d2e8
YasmineRaef <yasmineraef76@gmail.com>
2023-12-04 19:15:28 +0200

    Modification in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app_2/app/app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('notes_box');
   runApp(const NotesApp());
 }
