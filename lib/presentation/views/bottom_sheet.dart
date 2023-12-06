cbe30dd03c981c14e23e996947f09a7ec97107f9
YasmineRaef <yasmineraef76@gmail.com>
2023-12-06 17:25:27 +0200

    adding validation to bottom sheet

--- lib/presentation/views/bottom_sheet.dart
+++ lib/presentation/views/bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:notes_app_2/presentation/resources/theme.dart';
 import 'package:notes_app_2/presentation/widgets/custom_text_field.dart';
 
 class BottomSheetView extends StatelessWidget {
@@ -8,33 +7,80 @@ class BottomSheetView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const TextFields(),
-        const Gap(50),
-        Container(
-            height: 55,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(16)),
-            child: Center(
-                child: Text("Add",
-                    style: Theme.of(context).textTheme.labelMedium)))
-      ],
+    return const AddNoteForm();
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          const TextFields(),
+          const Gap(30),
+          GestureDetector(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+            child: Container(
+                height: 55,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.circular(16)),
+                child: Center(
+                    child: Text("Add",
+                        style: Theme.of(context).textTheme.labelMedium))),
+          )
+        ],
+      ),
     );
   }
 }
 
-class TextFields extends StatelessWidget {
+class TextFields extends StatefulWidget {
   const TextFields({super.key});
 
   @override
+  State<TextFields> createState() => _TextFieldsState();
+}
+
+class _TextFieldsState extends State<TextFields> {
+  String? title, subtitle;
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        CustomTextField(hint: "Title"),
-        CustomTextField(hint: "Content", maxLines: 5),
+        CustomTextField(
+            hint: "Title",
+            onSaved: (value) {
+              title = value;
+            }),
+        CustomTextField(
+            maxLines: 5,
+            hint: "Content",
+            onSaved: (value) {
+              subtitle = value;
+            }),
       ],
     );
   }
--- lib/presentation/widgets/custom_text_field.dart
+++ lib/presentation/widgets/custom_text_field.dart
@@ -2,15 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  const CustomTextField(
+      {super.key, required this.hint, this.maxLines = 1, this.onSaved});
 
   final String hint;
   final int maxLines;
+
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       const Gap(24),
-      TextField(
+      TextFormField(
+          onSaved: onSaved,
+          validator: (value) {
+            if (value?.isEmpty ?? true) {
+              return 'Field is required';
+            } else {
+              return null;
+            }
+          },
           maxLines: maxLines,
           cursorColor: Colors.tealAccent,
           decoration: InputDecoration(
