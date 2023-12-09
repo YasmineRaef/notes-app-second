92196de216f77fc2c49a51626614a9c079c2c834
YasmineRaef <yasmineraef76@gmail.com>
2023-12-09 15:40:09 +0200

    Register Adaptor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app_2/app/app.dart';
+import 'package:notes_app_2/presentation/resources/note_model.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('notes_box');
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
